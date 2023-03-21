c1d6438ee1b76f437fc0310e4948df03c8de1862
diegovbmachado <diegobardo@hotmail.com>
2023-03-21 19:10:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ let screen = document.querySelector('#tela');
 document.querySelectorAll('.colorArea .color').forEach(item =>{
 item.addEventListener('click', colorClickEvent);
 });
-//so testando novamente numero de gits
+//mais um apenas....dois...
 
 screen.addEventListener('mousedown', mouseDownEvent);
 screen.addEventListener('mousemove', mouseMoveEvent);
